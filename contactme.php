f5e61c7f2727b517a4e0999f9885af12f6523491
Josh Danielson <37751792+j-danielson@users.noreply.github.com>
2023-01-17 12:45:39 -0800

    created rep
    Created repository for it162

---
+++ contactme.php
@@ -0,0 +1,17 @@
+<!-- index.php -->
+<?php include 'includes/header.php';?>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Contact Me</h2>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<!-- <aside>
+ <h3>Right Column</h3>
+</aside> -->
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php'?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- index.php -->
+<?php include 'includes/header.php';?>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="professional photo of me" />
+ <img src="images/phone.jpg" class="phone" alt="photo of me and some friends" />
+<h2>A Bit About Me</h2>
+<p>Growing up on a farm in "little town" Iowa, I moved to the big city, Seattle about 10 years ago. I am currently going through schoool for a web development and managing a bottle shop in West Seattle. My hobbies in my little free time is playing video games, playing with my Corgi Porkchop, and trying new beers!</p>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="photo of me at work charity event" />
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php'?>
